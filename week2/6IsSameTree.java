fea882f8df5b27d02aeecf7598385cffbbd1e074
haomingjue <haomingjue@outlook.com>
2022-12-27 23:35:27 -0500

    feat: is same tree

---
+++ week2/6IsSameTree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public class TreeNode {
+         int val;
+            TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+                this.right = right;
+            }
+        }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if ((p == null) ^ (q == null)) {
+            return false;
+        }
+        if (p == null && q == null) {
+            return true;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
